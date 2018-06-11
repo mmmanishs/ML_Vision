03cacd8d7e0f58572f6a237d8778e3a56e660de3
Manish Singh <mmmanishs@gmail.com>
2018-06-11 00:09:05 -0400

    working copy

---
+++ Vision/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Vision
+//
+//  Created by Manish Singh on 6/7/18.
+//  Copyright © 2018 Manish Singh. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController, ImageProcessorResultDelegate {
+    @IBOutlet weak var cameraView: CameraView!
+    @IBOutlet weak var infoView: UIView!
+    @IBOutlet weak var infoLabel: UILabel!
+    var frameExtractor: FrameExtractor!
+    let imageProcessorType1 = ImageProcessorType1()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        cameraView.imageProcessor = imageProcessorType1
+        cameraView.imageProcessor?.resultDelegate = self
+        DispatchQueue.main.async {
+            self.cameraView.start()
+        }
+    }
+
+//    override var shouldAutorotate: Bool {
+//        return true
+//    }
+    
+    func imageProcessorResult(imageProcessor: ImageProcessor, result: ImageProcessorResult) {
+        DispatchQueue.main.async {
+            self.infoLabel.text = result.info
+        }
+    }
+}
+
